1d3685526118fa704de300610a0e1fe93714b839
“Anatoli <humanities5050@gmail.com>
2024-01-19 20:40:01 +0300

    feat: complete 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+stream.on('data', (chunck) => {
+  console.log(chunck);
+});
+stream.on('error', (err) => {
+  console.log(err);
+});
